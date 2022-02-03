937385a73bbd4ad3a3d2811c9670053703f1a754
Suleyman Musayev <slmusayev@gmail.com>
2022-02-03 12:28:28 +0500

    add links to movie items to index.erb

--- app.rb
+++ app.rb
@@ -2,9 +2,20 @@
 
 #add sinatra library
 require 'sinatra'
+#load movie.rb file to get movie items
+require 'movie'
 
 #action after receiveing GET /movies
 get ('/movies') do
+    #create new array
+    @movies = []
+    #create first item
+    @movies[0] = Movie.new
+    @movies[0].title = "Jaws"
+    @movies[1] = Movie.new
+    @movies[1].title = "Alien"
+    @movies[2] = Movie.new
+    @movies[2].title = "Terminator 2"
     #open index.erb file
     erb :index
 end
