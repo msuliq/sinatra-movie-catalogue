9f9e075ddb3570aa34f301ae46d2f295dfdf8ac8
Suleyman Musayev <slmusayev@gmail.com>
2022-02-03 16:53:37 +0500

    add movie_store.rb script to add new catalogue items

---
+++ lib/movie_store.rb
@@ -0,0 +1,25 @@
+#script for storing added new items 
+#add YAML module to store input data
+require 'yaml/store'
+
+class MovieStore
+    #set default values for input data
+    def initialize(file_name)
+        #pathway to storage file
+        @store = YAML::Store.new(file_name)
+    end
+    #set method for saving a movie as catalogue item
+    def save(movie)
+        #required transaction call
+        @store.transaction do
+            #if the movie has not been assigned id yet
+            unless movie.id
+                #find largest id value and add 1
+                highest_id = @store.roots.max || 0
+                movie.id = highest_id + 1
+            end
+            #movie is stored with respective id value
+            @store[movie.id] = movie
+        end
+    end
+end
